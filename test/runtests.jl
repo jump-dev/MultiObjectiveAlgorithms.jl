14e206b337dfe06a2398cd990d5e9adbc48ea936
odow <o.dowson@gmail.com>
2023-01-13 20:38:47 +1300

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,9 @@
 #  v.2.0. If a copy of the MPL was not distributed with this file, You can
 #  obtain one at http://mozilla.org/MPL/2.0/.
 
+import Pkg
+
 if ENV["CI"] == "true"
-    import Pkg
     Pkg.pkg"add MathOptInterface#od/vector-optimization"
     Pkg.pkg"add JuMP#od/vector-optimization"
 end
