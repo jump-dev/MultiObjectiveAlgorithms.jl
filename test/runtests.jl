73ff53b9e5095dd5f107f7b71abf95a3c1dbd1f4
odow <o.dowson@gmail.com>
2023-01-13 20:06:26 +1300

    Reenable pkg in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,11 @@
 #  v.2.0. If a copy of the MPL was not distributed with this file, You can
 #  obtain one at http://mozilla.org/MPL/2.0/.
 
-# import Pkg
-# Pkg.pkg"add MathOptInterface#od/vector-optimization"
-# Pkg.pkg"add JuMP#od/vector-optimization"
+if ENV["CI"] == "true"
+    import Pkg
+    Pkg.pkg"add MathOptInterface#od/vector-optimization"
+    Pkg.pkg"add JuMP#od/vector-optimization"
+end
 
 using Test
 
