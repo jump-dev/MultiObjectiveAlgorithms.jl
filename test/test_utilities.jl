01572ff430f4f02a1f6d9e50a9486fee96aa23dd
odow <o.dowson@gmail.com>
2023-02-08 14:21:05 +1300

    Add even more tests

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -90,13 +90,21 @@ end
 
 function test_filter_nondominated_triple()
     x = Dict{MOI.VariableIndex,Float64}()
-    solutions = [
-        MOA.SolutionPoint(x, [0, 1, 1]),
-        MOA.SolutionPoint(x, [0, 2, 0]),
-        MOA.SolutionPoint(x, [1, 1, 1]),
-    ]
-    @test MOA.filter_nondominated(MOI.MIN_SENSE, solutions) == solutions[1:2]
-    @test MOA.filter_nondominated(MOI.MAX_SENSE, solutions) == solutions[2:3]
+    for p in MOA.Combinatorics.permutations(1:3)
+        solutions = [
+            MOA.SolutionPoint(x, [0, 1, 1][p]),
+            MOA.SolutionPoint(x, [0, 2, 0][p]),
+            MOA.SolutionPoint(x, [1, 1, 1][p]),
+        ]
+        # The permutation can change the ordering of the solutions that are
+        # returnned, so we can't use `@test min_sol == solutions[1:2]`
+        min_sol = MOA.filter_nondominated(MOI.MIN_SENSE, solutions)
+        @test solutions[1] in min_sol && solutions[2] in min_sol
+        @test length(min_sol) == 2
+        max_sol = MOA.filter_nondominated(MOI.MAX_SENSE, solutions)
+        @test solutions[2] in max_sol && solutions[3] in max_sol
+        @test length(max_sol) == 2
+    end
     return
 end
 
