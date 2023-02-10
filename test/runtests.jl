63a020dbf61f554bf0cc2642034c14dcce782144
Oscar Dowson <odow@users.noreply.github.com>
2023-02-11 10:37:16 +1300

    Update to MOI v1.12.0 (#23)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,6 @@
 #  v.2.0. If a copy of the MPL was not distributed with this file, You can
 #  obtain one at http://mozilla.org/MPL/2.0/.
 
-import Pkg
-
-if get(ENV, "CI", "false") == "true"
-    Pkg.pkg"add MathOptInterface#master"
-end
-
 using Test
 
 @testset "$file" for file in readdir(joinpath(@__DIR__, "algorithms"))
