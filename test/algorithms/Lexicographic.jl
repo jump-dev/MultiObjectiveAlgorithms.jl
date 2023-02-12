f48e17e3e16d17f8044c08b8b2efa61998918260
odow <o.dowson@gmail.com>
2023-02-13 11:25:26 +1300

    Improve coverage

--- test/algorithms/Lexicographic.jl
+++ test/algorithms/Lexicographic.jl
@@ -112,34 +112,46 @@ function test_knapsack_default()
 end
 
 function test_infeasible()
-    model = MOA.Optimizer(HiGHS.Optimizer)
-    MOI.set(model, MOA.Algorithm(), MOA.Lexicographic())
-    MOI.set(model, MOI.Silent(), true)
-    x = MOI.add_variables(model, 2)
-    MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
-    MOI.add_constraint(model, 1.0 * x[1] + 1.0 * x[2], MOI.LessThan(-1.0))
-    f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
-    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
-    MOI.optimize!(model)
-    @test MOI.get(model, MOI.TerminationStatus()) == MOI.INFEASIBLE
-    @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
-    @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    for flag in (true, false)
+        model = MOA.Optimizer(HiGHS.Optimizer)
+        MOI.set(
+            model,
+            MOA.Algorithm(),
+            MOA.Lexicographic(; all_permutations = flag),
+        )
+        MOI.set(model, MOI.Silent(), true)
+        x = MOI.add_variables(model, 2)
+        MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
+        MOI.add_constraint(model, 1.0 * x[1] + 1.0 * x[2], MOI.LessThan(-1.0))
+        f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
+        MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.INFEASIBLE
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
+        @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    end
     return
 end
 
 function test_unbounded()
-    model = MOA.Optimizer(HiGHS.Optimizer)
-    MOI.set(model, MOA.Algorithm(), MOA.Lexicographic())
-    MOI.set(model, MOI.Silent(), true)
-    x = MOI.add_variables(model, 2)
-    MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
-    f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
-    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
-    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-    MOI.optimize!(model)
-    @test MOI.get(model, MOI.TerminationStatus()) == MOI.DUAL_INFEASIBLE
-    @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
-    @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    for flag in (true, false)
+        model = MOA.Optimizer(HiGHS.Optimizer)
+        MOI.set(
+            model,
+            MOA.Algorithm(),
+            MOA.Lexicographic(; all_permutations = flag),
+        )
+        MOI.set(model, MOI.Silent(), true)
+        x = MOI.add_variables(model, 2)
+        MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
+        f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
+        MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.DUAL_INFEASIBLE
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
+        @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    end
     return
 end
 
