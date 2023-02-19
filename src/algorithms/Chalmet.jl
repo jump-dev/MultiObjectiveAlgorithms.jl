6f400131393f25505fb8b5a0542c87dbb2f69d1e
Gökhan <kofgokhan@gmail.com>
2023-02-19 17:40:10 +0300

    Add Chalmet algorithm

---
+++ src/algorithms/Chalmet.jl
@@ -0,0 +1,105 @@
+#  Copyright 2019, Oscar Dowson and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public License,
+#  v.2.0. If a copy of the MPL was not distributed with this file, You can
+#  obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+    Chalmet()
+`Chalmet` implements the algorithm of:
+...
+"""
+mutable struct Chalmet <: AbstractAlgorithm end
+
+function _solve_constrained_model(
+    model::Optimizer,
+    ::Chalmet,
+    rhs::Vector{Float64},
+)
+    f = MOI.Utilities.scalarize(model.f)
+    MOI.set(model.inner, MOI.ObjectiveFunction{typeof(sum(f))}(), sum(f))
+    constraints = [
+        MOI.add_constraint(model.inner, f[1], MOI.LessThan(rhs[1] - 1))
+        MOI.add_constraint(model.inner, f[2], MOI.LessThan(rhs[2] - 1))
+    ]
+    MOI.optimize!(model.inner)
+    MOI.delete.(model, constraints)
+    status = MOI.get(model.inner, MOI.TerminationStatus())
+    if !_is_scalar_status_optimal(status)
+        return status, nothing
+    end
+    variables = MOI.get(model.inner, MOI.ListOfVariableIndices())
+    X, Y = _compute_point(model, variables, model.f)
+    return status, SolutionPoint(X, Y)
+end
+
+function optimize_multiobjective!(algorithm::Chalmet, model::Optimizer)
+    if MOI.output_dimension(model.f) != 2
+        error("Chalmet requires exactly two objectives")
+    end
+    if MOI.output_dimension(model.f) == 1
+    end
+    sense = MOI.get(model.inner, MOI.ObjectiveSense())
+    if sense == MOI.MAX_SENSE
+        old_obj, neg_obj = copy(model.f), -model.f
+        MOI.set(model, MOI.ObjectiveFunction{typeof(neg_obj)}(), neg_obj)
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+        status, solutions = optimize_multiobjective!(algorithm, model)
+        MOI.set(model, MOI.ObjectiveFunction{typeof(old_obj)}(), old_obj)
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+        if solutions !== nothing
+            solutions = [SolutionPoint(s.x, -s.y) for s in solutions]
+        end
+        return status, solutions
+    end
+    solutions = SolutionPoint[]
+    E = Tuple{Int,Int}[]
+    Q = Tuple{Int,Int}[]
+    variables = MOI.get(model.inner, MOI.ListOfVariableIndices())
+    f1, f2 = MOI.Utilities.scalarize(model.f)
+    y1, y2 = zeros(2), zeros(2)
+    MOI.set(model.inner, MOI.ObjectiveFunction{typeof(f2)}(), f2)
+    MOI.optimize!(model.inner)
+    status = MOI.get(model.inner, MOI.TerminationStatus())
+    if !_is_scalar_status_optimal(status)
+        return status, nothing
+    end
+    _, y1[2] = _compute_point(model, variables, f2)
+    MOI.set(model.inner, MOI.ObjectiveFunction{typeof(f1)}(), f1)
+    y1_constraint = MOI.add_constraint(model.inner, f2, MOI.LessThan(y1[2]))
+    MOI.optimize!(model.inner)
+    x1, y1[1] = _compute_point(model, variables, f1)
+    MOI.delete(model.inner, y1_constraint)
+    push!(solutions, SolutionPoint(x1, y1))
+    MOI.set(model.inner, MOI.ObjectiveFunction{typeof(f1)}(), f1)
+    MOI.optimize!(model.inner)
+    status = MOI.get(model.inner, MOI.TerminationStatus())
+    if !_is_scalar_status_optimal(status)
+        return status, nothing
+    end
+    _, y2[1] = _compute_point(model, variables, f1)
+    if y2[1] ≈ solutions[1].y[1]
+        return MOI.OPTIMAL, [solutions]
+    end
+    MOI.set(model.inner, MOI.ObjectiveFunction{typeof(f2)}(), f2)
+    y2_constraint = MOI.add_constraint(model.inner, f1, MOI.LessThan(y2[1]))
+    MOI.optimize!(model.inner)
+    x2, y2[2] = _compute_point(model, variables, f2)
+    MOI.delete(model.inner, y2_constraint)
+    push!(solutions, SolutionPoint(x2, y2))
+    push!(Q, (1, 2))
+    t = 3
+    while !isempty(Q)
+        r, s = pop!(Q)
+        yr, ys = solutions[r].y, solutions[s].y
+        rhs = [max(yr[1], ys[1]), max(yr[2], ys[2])]
+        status, solution = _solve_constrained_model(model, algorithm, rhs)
+        if !_is_scalar_status_optimal(status)
+            push!(E, (r, s))
+            continue
+        end
+        push!(solutions, solution)
+        append!(Q, [(r, t), (t, s)])
+        t += 1
+    end
+    return MOI.OPTIMAL, solutions
+end
---
+++ test/algorithms/Chalmet.jl
@@ -0,0 +1,161 @@
+#  Copyright 2019, Oscar Dowson and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public License,
+#  v.2.0. If a copy of the MPL was not distributed with this file, You can
+#  obtain one at http://mozilla.org/MPL/2.0/.
+
+module TestChalmet
+
+using Test
+
+import HiGHS
+import MultiObjectiveAlgorithms as MOA
+
+const MOI = MOA.MOI
+
+function run_tests()
+    if Sys.WORD_SIZE == 32
+        return  # Skip on 32-bit because HiGHS fails
+    end
+    for name in names(@__MODULE__; all = true)
+        if startswith("$name", "test_")
+            @testset "$name" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    return
+end
+
+function test_knapsack_min()
+    n = 10
+    W = 2137.0
+    C = Float64[
+        566 611 506 180 817 184 585 423 26 317
+        62 84 977 979 874 54 269 93 881 563
+    ]
+    w = Float64[557, 898, 148, 63, 78, 964, 246, 662, 386, 272]
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Chalmet())
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, n)
+    MOI.add_constraint.(model, x, MOI.ZeroOne())
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(
+            [MOI.ScalarAffineTerm(w[j], x[j]) for j in 1:n],
+            0.0,
+        ),
+        MOI.LessThan(W),
+    )
+    f = MOI.VectorAffineFunction(
+        [
+            MOI.VectorAffineTerm(i, MOI.ScalarAffineTerm(-C[i, j], x[j]))
+            for i in 1:2 for j in 1:n
+        ],
+        [0.0, 0.0],
+    )
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.optimize!(model)
+    X_E = Float64[
+        0 0 1 1 1 0 1 1 1 1
+        1 0 1 1 1 0 1 1 0 1
+        0 1 1 1 1 0 1 0 1 1
+    ]
+    Y_N = Float64[
+        -2854 -4636
+        -3394 -3817
+        -3042 -4627
+    ]
+    N = MOI.get(model, MOI.ResultCount())
+    x_sol = hcat([MOI.get(model, MOI.VariablePrimal(i), x) for i in 1:N]...)
+    @test isapprox(x_sol, X_E'; atol = 1e-6)
+    y_sol = hcat([MOI.get(model, MOI.ObjectiveValue(i)) for i in 1:N]...)
+    @test isapprox(y_sol, Y_N'; atol = 1e-6)
+    return
+end
+
+function test_knapsack_max()
+    n = 10
+    W = 2137.0
+    C = Float64[
+        566 611 506 180 817 184 585 423 26 317
+        62 84 977 979 874 54 269 93 881 563
+    ]
+    w = Float64[557, 898, 148, 63, 78, 964, 246, 662, 386, 272]
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Chalmet())
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, n)
+    MOI.add_constraint.(model, x, MOI.ZeroOne())
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(
+            [MOI.ScalarAffineTerm(w[j], x[j]) for j in 1:n],
+            0.0,
+        ),
+        MOI.LessThan(W),
+    )
+    f = MOI.VectorAffineFunction(
+        [
+            MOI.VectorAffineTerm(i, MOI.ScalarAffineTerm(C[i, j], x[j])) for
+            i in 1:2 for j in 1:n
+        ],
+        [0.0, 0.0],
+    )
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.optimize!(model)
+    X_E = Float64[
+        0 0 1 1 1 0 1 1 1 1
+        1 0 1 1 1 0 1 1 0 1
+        0 1 1 1 1 0 1 0 1 1
+    ]
+    Y_N = Float64[
+        2854 4636
+        3394 3817
+        3042 4627
+    ]
+    N = MOI.get(model, MOI.ResultCount())
+    x_sol = hcat([MOI.get(model, MOI.VariablePrimal(i), x) for i in 1:N]...)
+    @test isapprox(x_sol, X_E'; atol = 1e-6)
+    y_sol = hcat([MOI.get(model, MOI.ObjectiveValue(i)) for i in 1:N]...)
+    @test isapprox(y_sol, Y_N'; atol = 1e-6)
+    return
+end
+
+function test_unbounded()
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Chalmet())
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, 2)
+    MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
+    f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    MOI.optimize!(model)
+    @test MOI.get(model, MOI.TerminationStatus()) == MOI.DUAL_INFEASIBLE
+    @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
+    @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    return
+end
+
+function test_infeasible()
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Chalmet())
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, 2)
+    MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
+    MOI.add_constraint(model, 1.0 * x[1] + 1.0 * x[2], MOI.LessThan(-1.0))
+    f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.optimize!(model)
+    @test MOI.get(model, MOI.TerminationStatus()) == MOI.INFEASIBLE
+    @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
+    @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    return
+end
+
+end
+
+TestChalmet.run_tests()
