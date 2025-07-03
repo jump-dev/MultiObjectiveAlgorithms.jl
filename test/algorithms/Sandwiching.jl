99dbc12956e0c6bc8cc53f34c62e208a9875845e
Gökhan Kof <kofgokhan@gmail.com>
2025-07-03 22:40:29 +0300

    Added tests for no bounding box and time limit

--- test/algorithms/Sandwiching.jl
+++ test/algorithms/Sandwiching.jl
@@ -134,6 +134,61 @@ function test_unbounded()
     return
 end
 
+function test_no_bounding_box()
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Sandwiching(0.0))
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, 2)
+    MOI.add_constraint.(model, x, MOI.GreaterThan(0.0))
+    f = MOI.Utilities.operate(vcat, Float64, 1.0 .* x...)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    @test_logs (:warn,) MOI.optimize!(model)
+    @test MOI.get(model, MOI.TerminationStatus()) == MOI.DUAL_INFEASIBLE
+    @test MOI.get(model, MOI.PrimalStatus()) == MOI.NO_SOLUTION
+    @test MOI.get(model, MOI.DualStatus()) == MOI.NO_SOLUTION
+    return
+end
+
+function test_time_limit()
+    p = 3
+    n = 10
+    W = 2137.0
+    C = Float64[
+        566 611 506 180 817 184 585 423 26 317
+        62 84 977 979 874 54 269 93 881 563
+        664 982 962 140 224 215 12 869 332 537
+    ]
+    w = Float64[557, 898, 148, 63, 78, 964, 246, 662, 386, 272]
+    model = MOA.Optimizer(HiGHS.Optimizer)
+    MOI.set(model, MOA.Algorithm(), MOA.Sandwiching(0.0))
+    MOI.set(model, MOI.TimeLimitSec(), 0.0)
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, n)
+    MOI.add_constraint.(model, x, MOI.ZeroOne())
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(
+            [MOI.ScalarAffineTerm(w[j], x[j]) for j in 1:n],
+            0.0,
+        ),
+        MOI.LessThan(W),
+    )
+    f = MOI.VectorAffineFunction(
+        [
+            MOI.VectorAffineTerm(i, MOI.ScalarAffineTerm(-C[i, j], x[j]))
+            for i in 1:p for j in 1:n
+        ],
+        fill(0.0, p),
+    )
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.optimize!(model)
+    @test MOI.get(model, MOI.TerminationStatus()) == MOI.TIME_LIMIT
+    @test MOI.get(model, MOI.ResultCount()) == size(C, 1) # anchor points are already computed when the time limit is checked
+    return
+end
+
 end  # TestSandwiching
 
 TestSandwiching.run_tests()
