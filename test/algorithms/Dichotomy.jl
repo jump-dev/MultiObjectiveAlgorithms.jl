16542fb01b8ddbfb7ee7642ee35a09cabad1d6fe
Oscar Dowson <odow@users.noreply.github.com>
2025-08-20 16:47:33 +1200

    Move time limit check to solve loop in Dichotomy (#149)

--- test/algorithms/Dichotomy.jl
+++ test/algorithms/Dichotomy.jl
@@ -262,7 +262,7 @@ function test_time_limit()
     )
     MOI.optimize!(model)
     @test MOI.get(model, MOI.TerminationStatus()) == MOI.TIME_LIMIT
-    @test MOI.get(model, MOI.ResultCount()) == 0
+    @test MOI.get(model, MOI.ResultCount()) == 2
     return
 end
 
