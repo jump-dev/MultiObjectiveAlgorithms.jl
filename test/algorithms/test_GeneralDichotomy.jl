a29cfa8dc63036c295ca5e73b499286e16b32ea0
Samuel Buchet <samuel.buchet@inrae.fr>
2026-06-24 15:28:39 +0200

    add bioobjective knapsack test

--- test/algorithms/test_GeneralDichotomy.jl
+++ test/algorithms/test_GeneralDichotomy.jl
@@ -21,16 +21,13 @@ function run_tests()
 end
 
 
-function test_lap()
-
-    # toy instance from ?
+function test_lap() # toy instance from (Przybylski et al., 2019)
     costs = zeros(Float64, (3, 4, 4))
     costs[1,:,:] = [3 6 4 5; 2 3 5 4; 3 5 4 2; 4 5 3 6]
     costs[2,:,:] = [2 3 5 4; 5 3 4 3; 5 2 6 4; 4 5 2 5]
     costs[3,:,:] = [4 2 4 2; 4 2 4 6; 4 2 6 3; 2 4 5 3]
     n = costs.size[2] # 4 variables
     d = costs.size[1] # 3 objectives
-
     model = MOA.Optimizer(HiGHS.Optimizer)
     x_ = MOI.add_variables(model, n * n)
     x = reshape(x_, n, n)
@@ -40,54 +37,56 @@ function test_lap()
     f = MOI.Utilities.operate(vcat, Float64, sum(costs[1,:,:] .* x), sum(costs[2,:,:] .* x), sum(costs[3,:,:] .* x))
     MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
     MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-
-
-    println("model: ")
-    println(model)
-
     precision = 6
-
     algorithm = MOA.GeneralDichotomy(precision)
-    algorithm.verbose = 1
-
     MOI.set(model, MOA.Algorithm(), algorithm)
-
-    # MOI.set(optimizer, MOA.SolutionLimit(), 3)
-
-    println("Costs size: ", costs.size)
-
-    # disable model log
-    # set_attribute(model, MOI.Silent(), true)
-
     MOI.optimize!(model)
-
     solve_time = MOI.get(model, MOI.SolveTimeSec())
-    @test  MOI.get(model, MOI.ResultCount()) == 10 
-
-    println("Number of solutions: ", MOI.get(model, MOI.ResultCount()))
-    println("solve time: ", solve_time)
-    println("solve time inner: ", model.solve_time_inner)
-
-    generaldichotomy_time = model.solve_time-model.solve_time_inner
-    println("GeneralDichotomy time: ", generaldichotomy_time, " s")
-
-    println("n weights: ", algorithm.weights.size)
-    println("n intermediate weights: ", algorithm.n_interm_weights)
-    println("N solved scalarization: ", algorithm.n_call_solve)
-
-    println("Objective cost vectors")
-    for i in 1:MOI.get(model, MOI.ResultCount())
-        y = MOI.get(model, MOI.ObjectiveValue(i))
-        println(y)
-    end
-
+    @test  MOI.get(model, MOI.ResultCount()) == 4 
 end
 
-# test on knapsack
-
-# test on bi-objective problem
-
-# test on min and max
+function test_biobjective_knapsack() # from the Dichotomy test set
+    p1 = [77, 94, 71, 63, 96, 82, 85, 75, 72, 91, 99, 63, 84, 87, 79, 94, 90]
+    p2 = [65, 90, 90, 77, 95, 84, 70, 94, 66, 92, 74, 97, 60, 60, 65, 97, 93]
+    w = [80, 87, 68, 72, 66, 77, 99, 85, 70, 93, 98, 72, 100, 89, 67, 86, 91]
+    precision = 3
+    general_dichotomy = MOA.GeneralDichotomy(precision)
+    general_dichotomy.verbose = 0
+    f = MOI.OptimizerWithAttributes(
+        () -> MOA.Optimizer(HiGHS.Optimizer),
+        MOA.Algorithm() => general_dichotomy,
+    )
+    model = MOI.instantiate(f)
+    MOI.set(model, MOI.Silent(), true)
+    x = MOI.add_variables(model, length(w))
+    MOI.add_constraint.(model, x, MOI.ZeroOne())
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    f = MOI.Utilities.operate(
+        vcat,
+        Float64,
+        [sum(1.0 * p[i] * x[i] for i in 1:length(w)) for p in [p1, p2]]...,
+    )
+    MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+    MOI.add_constraint(
+        model,
+        sum(1.0 * w[i] * x[i] for i in 1:length(w)),
+        MOI.LessThan(900.0),
+    )
+    MOI.optimize!(model)
+    results = [
+        [918.0, 983.0] => [2, 3, 4, 5, 6, 8, 10, 11, 12, 16, 17],
+        [934.0, 971.0] => [2, 3, 5, 6, 8, 10, 11, 12, 15, 16, 17],
+        [948.0, 939.0] => [1, 2, 3, 5, 6, 8, 10, 11, 15, 16, 17],
+        [955.0, 906.0] => [2, 3, 5, 6, 9, 10, 11, 14, 15, 16, 17],
+    ]
+    reverse!(results)
+    for i in 1:MOI.get(model, MOI.ResultCount())
+        x_sol = MOI.get(model, MOI.VariablePrimal(i), x)
+        @test results[i][2] == findall(elt -> elt > 0.9, x_sol)
+        @test results[i][1] ≈ MOI.get(model, MOI.ObjectiveValue(i))
+    end
+    return
+end
 
 end  # module TestGeneralDichotomy
 
