6b86bd0c2d3592f8d3a3eb1982e97287ffea5562
Oscar Dowson <o.dowson@gmail.com>
2026-06-30 12:44:42 +1200

    Re-enable tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,16 @@
 #  v.2.0. If a copy of the MPL was not distributed with this file, You can
 #  obtain one at http://mozilla.org/MPL/2.0/.
 
-include("algorithms/test_GeneralDichotomy.jl")
+is_test_file(f) = startswith(f, "test_") && endswith(f, ".jl")
 
-# is_test_file(f) = startswith(f, "test_") && endswith(f, ".jl")
+testsuite = Dict{String,Expr}()
+for (root, dirs, files) in walkdir(@__DIR__)
+    for file in joinpath.(root, filter(is_test_file, files))
+        testsuite[file] = :(include($file))
+    end
+end
 
-# testsuite = Dict{String,Expr}()
-# for (root, dirs, files) in walkdir(@__DIR__)
-#     for file in joinpath.(root, filter(is_test_file, files))
-#         testsuite[file] = :(include($file))
-#     end
-# end
+import MultiObjectiveAlgorithms
+import ParallelTestRunner
 
-# import MultiObjectiveAlgorithms
-# import ParallelTestRunner
-
-# ParallelTestRunner.runtests(MultiObjectiveAlgorithms, ARGS; testsuite)
+ParallelTestRunner.runtests(MultiObjectiveAlgorithms, ARGS; testsuite)
