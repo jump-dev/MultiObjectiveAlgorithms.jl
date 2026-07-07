efc64ca8ceb2a4fe2490a380566dba1ef6156f91
Oscar Dowson <odow@users.noreply.github.com>
2026-07-07 12:23:10 +1200

    Modify silent mode settings in test function
    Comment out the silent mode setting and add silent inner mode setting.

--- test/algorithms/test_EpsilonConstraint.jl
+++ test/algorithms/test_EpsilonConstraint.jl
@@ -26,7 +26,8 @@ end
 function test_vOptLib_runtests()
     model = MOA.Optimizer(HiGHS.Optimizer)
     MOI.set(model, MOA.Algorithm(), MOA.EpsilonConstraint())
-    MOI.set(model, MOI.Silent(), true)
+    # MOI.set(model, MOI.Silent(), true)
+    MOI.set(model, MOA.SilentInner(), false)
     vOptLib.run_tests(model)
     return
 end
